aedf03b544b8706c3f334a4fe2baabe9d5a7c921
Illia Dovhaliuk <2201illia@gmail.com>
2023-12-07 14:34:12 +0200

    make changes

---
+++ src/components/header/header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// ... Continue styling the rest of your components
+
+const Header = () => {
+  return (
+    <HeaderWrapper>
+      <Container>
+        {/* Replace class names with styled components */}
+        <Logo>{/* Logo content */}</Logo>
+        {/* ... Continue with other header components */}
+      </Container>
+    </HeaderWrapper>
+  );
+};
+
+export default Header;
